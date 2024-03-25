d80f735af8b029d4b8821835a63d522f5bef7443
Maxime RONCERAY <maxime.ronceray@lpp.polytechnique.fr>
2024-03-25 10:49:42 +0100

    update unit_tests

--- tests/gui_test.py
+++ tests/gui_test.py
@@ -5,10 +5,7 @@ import os
 import json
 from PyQt6.QtWidgets import QApplication
 
-# Ensure the import path matches your project structure
 from src.view.gui import MainGUI
-
-# Ensure a QApplication instance exists for widget creation
 app = QApplication.instance() or QApplication(sys.argv)
 
 
@@ -17,10 +14,8 @@ class TestMainGUI(unittest.TestCase):
     def setUpClass(cls):
         super(TestMainGUI, cls).setUpClass()
         cls.real_data_path = os.path.join(os.getcwd(), 'test_data', 'real_data.json')
-        # Ensure the path to real_data.json is correct for your project
 
     def setUp(self):
-        # Initialize MainGUI for each test
         self.gui = MainGUI()
 
     @patch('PyQt6.QtWidgets.QFileDialog.getSaveFileName')
@@ -29,14 +24,12 @@ class TestMainGUI(unittest.TestCase):
         # Use the real data file path for the mock dialog return value
         mock_save_dialog.return_value = (self.real_data_path, 'JSON Files (*.json)')
 
-        # Assuming export_parameters_to_json modifies or uses self.input_parameters
         with open(self.real_data_path, 'r') as file:
             self.gui.input_parameters = json.load(file)
 
         # Perform export
         self.gui.export_parameters_to_json()
 
-        # Verify the file exists and check its content (this part depends on how export_parameters_to_json is implemented)
         self.assertTrue(os.path.exists(self.real_data_path))
         with open(self.real_data_path, 'r') as exported_file:
             data = json.load(exported_file)
